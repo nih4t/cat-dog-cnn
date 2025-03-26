2c6ac6a1b3f0897a168bd3fba63293d2ddf2dc2c
Nihat Akkaya <nihatkky@gmail.com>
2025-03-26 23:06:28 +0100

    Convolution

--- main.py
+++ main.py
@@ -22,3 +22,22 @@ test_set = test_datagen.flow_from_directory(
     target_size=(64, 64),
     batch_size=32,
     class_mode='binary')
+
+
+# Convolution
+cnn = tf.keras.models.Sequential()
+cnn.add(tf.keras.layers.Conv2D(filters=32, kernel_size=3, activation='relu', input_shape=[64, 64, 3]))
+
+# Pooling
+cnn.add(tf.keras.layers.MaxPool2D(pool_size=2, strides=2))
+
+# 2.Convolution
+cnn = tf.keras.models.Sequential()
+cnn.add(tf.keras.layers.Conv2D(filters=32, kernel_size=3, activation='relu'))
+
+# 2. Pooling
+cnn.add(tf.keras.layers.MaxPool2D(pool_size=2, strides=2))
+
+cnn.add(tf.keras.Flatten())
+
+
